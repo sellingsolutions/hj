c303f1d54c230cfc9cd1d688b7576c08d8ba472f
zno <alexander@icontrolapp.se>
2021-09-21 11:27:28 +0200

    28: Create a hubspotdeal whenever a warranty reminder notification is sent

--- HubspotConnector.Tests/HubspotApiClientTests.cs
+++ HubspotConnector.Tests/HubspotApiClientTests.cs
@@ -1,10 +1,20 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using Skarp.HubSpotClient.Associations;
+using Skarp.HubSpotClient.Associations.Dto;
+using Skarp.HubSpotClient.Company;
+using Skarp.HubSpotClient.Company.Dto;
 using Skarp.HubSpotClient.Contact;
 using Skarp.HubSpotClient.Contact.Dto;
+using Skarp.HubSpotClient.Core;
 using Skarp.HubSpotClient.Deal;
 using Skarp.HubSpotClient.Deal.Dto;
+using Skarp.HubSpotClient.Owner;
+using Skarp.HubSpotClient.Owner.Dto;
 
 namespace HubspotConnector.Tests
 {
@@ -20,8 +30,62 @@ namespace HubspotConnector.Tests
         public async Task GetContact()
         {
             var client = new HubSpotContactClient("ec4e818f-8961-4d91-96df-ac72de5a1cdc");
-            var contact = await client.GetByEmailAsync<ContactHubSpotEntity>("mikael.palm@einarmattsson.se");
+
+            var contact = await client.GetByEmailAsync<ContactHubSpotEntity>("lovisa.hallman@spg.se", new ContactGetRequestOptions
+            {
+                IncludeListMemberships = true,
+                IncludeHistory = true
+            });
             Assert.IsNotNull(contact);
+        }
+        
+        [Test]
+        public async Task GetAssociations()
+        {
+            var contactClient = new HubSpotContactClient("ec4e818f-8961-4d91-96df-ac72de5a1cdc");
+
+            var contact = await contactClient.GetByEmailAsync<ContactHubSpotEntity>("lovisa.hallman@spg.se", new ContactGetRequestOptions
+            {
+                IncludeListMemberships = true,
+                IncludeHistory = true
+            });
+            
+            var client = new HubSpotAssociationsClient("ec4e818f-8961-4d91-96df-ac72de5a1cdc");
+            var associations = await client.GetListByIdAsync(
+                contact.Id ?? 0,
+                HubSpotAssociationDefinitions.ContactToCompany,
+                new AssociationListRequestOptions
+                {
+                    NumberOfAssociationsToReturn = 1
+                });
+            
+            var companyClient = new HubSpotCompanyClient("ec4e818f-8961-4d91-96df-ac72de5a1cdc");
+            var company = await companyClient.GetByIdAsync<CompanyHubSpotEntity>(associations.Results.FirstOrDefault());
+            
+            Assert.IsNotNull(company);
+        }
+        
+        [Test]
+        public async Task GetOwner()
+        {
+            var client = new HubSpotOwnerClient("ec4e818f-8961-4d91-96df-ac72de5a1cdc");
+            var ownerList = await client.ListAsync<OwnerHubSpotEntity>(new OwnerListRequestOptions
+            {
+                Email = "yones@besiktningsman.se"
+            });
+            
+            Assert.IsNotNull(ownerList);
+        }
+        
+        [Test]
+        public async Task GetCompany()
+        {
+            var client = new HubSpotCompanyClient("ec4e818f-8961-4d91-96df-ac72de5a1cdc");
+            var company = await client.GetByIdAsync<CompanyHubSpotEntity>(3285005450);
+            
+            var duplicate = await client.CreateAsync<CompanyHubSpotEntity>(company);
+
+            Assert.IsNotNull(company);
         }
         
         [Test]
