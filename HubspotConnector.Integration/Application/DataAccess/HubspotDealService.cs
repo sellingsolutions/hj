feed39b01c58aa19473227273d9ba7250cd14469
zno <alexander@icontrolapp.se>
2021-09-21 11:27:28 +0200

    23: Create a hubspotdeal whenever a warranty reminder notification is sent

---
+++ HubspotConnector.Integration/Application/DataAccess/HubspotDealService.cs
@@ -0,0 +1,11 @@
+namespace HubspotConnector.Integration.Application.DataAccess
+{
+    public class HubspotDealService : IHubspotDealService
+    {
+        private readonly 
+        public HubspotDealService()
+        {
+            
+        }
+    }
+}
