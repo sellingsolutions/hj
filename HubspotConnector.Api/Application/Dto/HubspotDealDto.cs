2a6bbe798f466ad2bf3b74a4a293d91600677a5e
zno <alexander@icontrolapp.se>
2021-09-21 11:27:27 +0200

    15: Create a hubspotdeal whenever a warranty reminder notification is sent

---
+++ HubspotConnector.Api/Application/Dto/HubspotDealDto.cs
@@ -0,0 +1,19 @@
+
+using System.Runtime.Serialization;
+using Skarp.HubSpotClient.Deal.Dto;
+
+namespace HubspotConnector.Application.Dto
+{
+    [DataContract]
+    public class HubspotDealDto : DealHubSpotEntity
+    {
+        [DataMember(Name="ispect-url")]
+        public string Url {get; set;}
+        
+        [DataMember(Name="antal_lagenheter")]
+        public long NoOfApartments { get; set; }
+        
+        [DataMember(Name="antal_lagenheter_flerbostadshus")]
+        public long NoOfApartmentBuildings { get; set; }
+    }
+}
